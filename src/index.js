b1cca062c3dc24be409c53e9fc5113f398a12b34
Natalia Orsi <naty.m.orsi@gmail.com>
2023-12-01 10:48:28 +0100

    Added Javascript to the project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function generatePoem (event) {
+    event.preventDefault();
+
+    new Typewriter("#poem", {
+      strings: ["La tombe dit a la Rose"],
+      autoStart: true,
+      delay: 20,
+      cursor:"",
+    });
+    
+}
+
+let formElement = document.querySelector("#poem-generator-form");
+
+formElement.addEventListener("submit", generatePoem);
