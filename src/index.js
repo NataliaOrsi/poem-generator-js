f0df85a7506c2e86be3dde05cf0166d92904a9f0
Natalia Orsi <naty.m.orsi@gmail.com>
2023-12-05 11:13:32 +0100

    Added the AI instructions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+function displayPoem (response) {
+
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+
+}
+
 function generatePoem (event) {
     event.preventDefault();
 
-    new Typewriter("#poem", {
-      strings: "La tombe dit a la Rose",
-      autoStart: true,
-      delay: 20,
-      cursor:"",
-    });
+    let apiKey = "5b303bcbffcb9458t34a63o545f3d664";
+    let instructions = document.querySelector("#user-instructions")
+    let prompt = `User instructions: Generate a poem about ${instructions.value}`;
+    let context =
+      "You're a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
     
 }
 
