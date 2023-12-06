fdb477c08c7f774c4d614367c19d7b6ef1960a5e
Natalia Orsi <naty.m.orsi@gmail.com>
2023-12-06 13:53:26 +0100

    updated styles

--- src/index.js
+++ src/index.js
@@ -16,8 +16,12 @@ function generatePoem (event) {
     let instructions = document.querySelector("#user-instructions")
     let prompt = `User instructions: Generate a poem about ${instructions.value}`;
     let context =
-      "You're a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Do NOT include the poem title";
+      "You're a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with <br />. Make sure to follow the user instructions. Make sure to NOT include the poem title";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+     let poemElement = document.querySelector("#poem");
+     poemElement.classList.remove("hidden");
+     poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructions.value}</div>`;
 
     axios.get(apiUrl).then(displayPoem);
     
